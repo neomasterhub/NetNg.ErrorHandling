85e79ef1ca3d12ab5c51c732236e5249ef7d5349
Neomaster <the@neomaster.net>
2023-03-05 11:41:07 +0300

    [WebApi] TestController

---
+++ WebApi/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+public class TestController : ApiControllerBase
+{
+    [HttpGet]
+    public string Index()
+    {
+        return "Hello World!";
+    }
+}
