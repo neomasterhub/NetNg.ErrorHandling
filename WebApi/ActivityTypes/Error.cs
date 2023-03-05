3aafe99ed52026dfa36454394b0dd237f3643cfa
Neomaster <the@neomaster.net>
2023-03-05 15:37:15 +0300

    [WebApi] Error

---
+++ WebApi/ActivityTypes/Error.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApi.ActivityTypes;
+
+public class Error
+{
+    public Error(Exception e)
+    {
+        Type = e.GetType().Name;
+        Message = e.Message;
+    }
+
+    public string Type { get; }
+    public string Message { get; }
+}
