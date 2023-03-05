1c9f5c026969cd3c7593830ac3c10489f7651317
Neomaster <the@neomaster.net>
2023-03-05 11:37:00 +0300

    [WebApi] CorsPolicyConfigurator

---
+++ WebApi/Configurators/CorsPolicyConfigurator.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApi.Configurators;
+
+public static class CorsPolicyConfigurator
+{
+    public static readonly string CorsPolicyName = "error handling web API";
+
+    public static IServiceCollection AddCorsPolicy(this IServiceCollection services) => services
+        .AddCors(options => options.AddPolicy(
+            CorsPolicyName,
+            builder => builder
+                .WithOrigins("http://localhost:4200")
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials()));
+}
